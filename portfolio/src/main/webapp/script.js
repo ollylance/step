c4d4b22c46ce1b38c621b8329f7ead0e58547384
Oliver Lance <olly.lance15@gmail.com>
2020-05-28 14:04:19 -0400

    add functionality to gallery (clickable imgs)

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -15,6 +15,56 @@
 /**
  * Adds a random greeting to the page.
  */
+
+function resizeOverlay(){
+    const background = document.getElementById("background-overlay");
+    const img = document.getElementById("enlarged-img"); 
+    img.style.maxHeight = (window.innerHeight * .75) + "px";
+    img.style.top = window.pageYOffset + "px";
+    img.style.maxWidth =  "60%";
+    background.style.width = "100%";
+    background.style.height = window.innerHeight + "px";
+    background.style.top = window.pageYOffset + "px";
+    background.style.left = 0;
+}
+
+function openImg(imgURL){
+    const background = document.getElementById("background-overlay");
+    background.style.backgroundColor = "rgba(71, 71, 71, .8)";
+    background.style.width = "100%";
+    background.style.height = window.innerHeight + "px";
+    background.style.top = window.pageYOffset + "px";
+    background.style.left = 0;
+    background.style.zIndex = 2;
+
+    const img = document.getElementById("enlarged-img"); 
+    img.src = imgURL;
+    img.alt = "enlarged gallery image";
+    img.style.width = "auto";
+    img.style.height = "auto";
+    img.style.maxWidth =  "60%";
+    img.style.maxHeight = (window.innerHeight * .75) + "px";
+    img.style.top = window.pageYOffset + "px";
+    img.style.zIndex = 3;
+
+    var body = document.getElementsByTagName("body");
+    body[0].style.overflow = "hidden";
+
+    window.addEventListener('resize', resizeOverlay);
+
+    background.addEventListener('click', function(){
+        background.style.width =  0;
+        background.style.height =  0;
+        background.style.zIndex = -1;
+        img.style.width = 0;
+        img.style.height = 0;
+        var body = document.getElementsByTagName("body");
+        body[0].style.overflow = "visible";
+        window.removeEventListener('resize', resizeOverlay);
+    });
+}
+
+
 function addRandomGreeting() {
   const greetings =
       ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
