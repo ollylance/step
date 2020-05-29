7f30a769feba868f1784587c39d192d553420e46
Oliver Lance <olly.lance15@gmail.com>
2020-05-29 09:49:56 -0400

    add more declarative way to enlarge images when clicked

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,52 +12,43 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
- * Adds a random greeting to the page.
- */
 
 function resizeOverlay(){
-    const background = document.getElementById("background-overlay");
-    const img = document.getElementById("enlarged-img"); 
+    const background = document.getElementById("background-clicked");
+    const img = document.getElementById("enlarged-clicked");
     img.style.maxHeight = (window.innerHeight * .75) + "px";
     img.style.top = window.pageYOffset + "px";
-    img.style.maxWidth =  "60%";
-    background.style.width = "100%";
     background.style.height = window.innerHeight + "px";
     background.style.top = window.pageYOffset + "px";
-    background.style.left = 0;
 }
 
-function openImg(imgURL){
+function openImg(elem){
+    imgURL = elem.src;
     const background = document.getElementById("background-overlay");
-    background.style.backgroundColor = "rgba(71, 71, 71, .8)";
-    background.style.width = "100%";
+    background.removeAttribute("id");
+    background.setAttribute("id", "background-clicked");
     background.style.height = window.innerHeight + "px";
     background.style.top = window.pageYOffset + "px";
-    background.style.left = 0;
-    background.style.zIndex = 2;
 
     const img = document.getElementById("enlarged-img"); 
     img.src = imgURL;
     img.alt = "enlarged gallery image";
-    img.style.width = "auto";
-    img.style.height = "auto";
-    img.style.maxWidth =  "60%";
+    img.removeAttribute("id");
+    img.setAttribute("id", "enlarged-clicked");
     img.style.maxHeight = (window.innerHeight * .75) + "px";
     img.style.top = window.pageYOffset + "px";
-    img.style.zIndex = 3;
-
+    
     var body = document.getElementsByTagName("body");
     body[0].style.overflow = "hidden";
 
     window.addEventListener('resize', resizeOverlay);
 
     background.addEventListener('click', function(){
-        background.style.width =  0;
-        background.style.height =  0;
-        background.style.zIndex = -1;
-        img.style.width = 0;
-        img.style.height = 0;
+        background.removeAttribute("id");
+        img.removeAttribute("id");
+        background.setAttribute("id", "background-overlay");
+        img.setAttribute("id", "enlarged-img");
+    
         var body = document.getElementsByTagName("body");
         body[0].style.overflow = "visible";
         window.removeEventListener('resize', resizeOverlay);
@@ -65,14 +56,12 @@ function openImg(imgURL){
 }
 
 
-function addRandomGreeting() {
-  const greetings =
+function addRandomQuotes() {
+  const quotes =
       ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
 
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+  const quote = quotes[Math.floor(Math.random() * greetings.length)];
 
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+  const quoteContainer = document.getElementById('quote-container');
+  quoteContainer.innerText = quote;
 }
