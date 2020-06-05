fe0cb704886668ff7d030a392da09bae5a989d3b
Oliver Lance <olly.lance15@gmail.com>
2020-06-05 18:15:44 +0000

    adds user authentication using google sign-in

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -164,7 +164,26 @@ function loadHTML(commentData){
 async function getComments(cursor, dir, reload){
     var sort = document.getElementById("sort").value;
     var numComments = document.getElementById("numComments").value;
-    const resp = await fetch('/data?reload='+reload+'&numComments='+numComments+'&sort='+sort+'&dir='+dir+cursor);
+    var auth2 = gapi.auth2.getAuthInstance();
+    var id = auth2.currentUser.get().getId();
+    const resp = await fetch('/data?id='+id+'&reload='+reload+'&numComments='+numComments+'&sort='+sort+'&dir='+dir+cursor);
     var commentData = await resp.json();
     loadHTML(commentData);
 }
+
+
+async function onSignIn(googleUser) {
+    // The ID token you need to pass to your backend:
+    gapi.load('auth2', function() {
+        var auth2 = gapi.auth2.init();
+    });
+    var id_token = googleUser.getAuthResponse().id_token;
+    const res = await fetch('/tokensignin?token_id='+id_token, {method:'POST'});
+}
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  }
